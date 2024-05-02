f3345a05b54598c81c51e01c9df74be52f03f3e5
Arjun Raj <arjunraj@One.local>
2024-05-03 00:58:28 +0530

    created basic structure

---
+++ android/app/src/main/kotlin/com/example/simplemenuapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.simplemenuapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:simplemenuapp/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dynamic Menu',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+const String menuJson = '''
+{
+  "Menu1": {
+    "submenu1": {
+      "innersub1": {
+        "Title": "inner title",
+        "body": "inner page description"
+      },
+      "innersub2": {
+        "Title": "inner title 2",
+        "body": "inner page 2 description"
+      }
+    },
+    "submenu2": {
+      "innersub": {
+        "Title": "inner title",
+        "body": "inner page description"
+      }
+    }
+  },
+  "Menu2": {
+    "submenu1": {
+      "Title": "inner title",
+      "body": "inner page description"
+    }
+  },
+  "Menu3": {
+ "submenu1": {
+ "Title": "inner title",
+ "body": "inner page description"
+ }
+ },
+ "Menu4": {
+ "submenu1": {
+ "Title": "inner title",
+ "body": "inner page description"
+ }
+ },
+ "Menu6": {
+ "submenu1": {
+ "Title": "inner title",
+ "body": "inner page description"
+ }
+ },
+ "Menu7": {
+ "submenu1": {"Title": "inner title",
+ "body": "inner page description"
+ }
+ },
+ "Menu8": {
+ "submenu1": {
+ "Title": "inner title",
+ "body": "inner page description"
+ }
+ }
+}
+
+''';
